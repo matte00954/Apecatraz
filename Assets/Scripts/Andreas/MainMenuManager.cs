86c44c2d76c65401c252c411073508d3a60f7492
TeaTiger <cfasch@gmail.com>
2021-11-11 12:58:47 +0100

    Update MainMenuManager.cs

--- Assets/Scripts/Andreas/MainMenuManager.cs
+++ Assets/Scripts/Andreas/MainMenuManager.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
-{   
+{
+    private void Awake()
+    {
+        Time.timeScale = 1;
+    }
     public void LoadPrototype(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
