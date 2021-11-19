26f0251dca41f270c8f3fae86b10ea4d959f9cc7
TeaTiger <cfasch@gmail.com>
2021-11-19 10:40:22 +0100

    Update To MainTest (and some vfx)

--- Assets/Scripts/Andreas/MonkeyVFX.cs
+++ Assets/Scripts/Andreas/MonkeyVFX.cs
@@ -21,7 +21,6 @@ public class MonkeyVFX : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!thirdPersonMovement.Moving)
         {
            dustTrail.Play();
         }
