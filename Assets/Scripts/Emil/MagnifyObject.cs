10541266de17b529b9de1f5a60d5f5fd699b9526
Smaksak <63716812+Smaksak@users.noreply.github.com>
2021-11-22 10:51:45 +0100

    Shader work

---
+++ Assets/Scripts/Emil/MagnifyObject.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnifyObject : MonoBehaviour
+{
+    Renderer rend;
+    Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 screenPoint = cam.WorldToScreenPoint(transform.position);
+        screenPoint.x = screenPoint.x / Screen.width;
+        screenPoint.y = screenPoint.y / Screen.height;
+        rend.material.SetVector("ObjScreenPos", screenPoint);
+    }
+}
