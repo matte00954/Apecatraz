8b3f8d2409e8f14c30d20730f6a9beb805f4c6ce
hannahellberg <82030714+hannahellberg@users.noreply.github.com>
2021-11-25 15:08:32 +0100

    Prototype script update

--- Assets/Scripts/Prototypes/ToggleOutline.cs
+++ Assets/Scripts/Prototypes/ToggleOutline.cs
@@ -5,39 +5,63 @@ using UnityEngine;
 public class ToggleOutline : MonoBehaviour
 {
     public GameObject[] outlines;
-    private bool isEnabled;
+    public GameObject[] highlights;
+
+    private bool outlineIsEnabled;
+    private bool highlightIsEnabled;
 
     // Start is called before the first frame update
     void Start()
     {
         turnOffOutlines();
+        turnOffHighlights();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.T) && !isEnabled)
+        if (Input.GetKeyDown(KeyCode.T) && !outlineIsEnabled)
         {
-            isEnabled = true;            
+            outlineIsEnabled = true;            
             foreach (GameObject outline in outlines)
             {
                 outline.SetActive(true);                
             } 
-            Debug.Log("Enable");           
+                       
         }
-        else if(Input.GetKeyDown(KeyCode.T) && isEnabled)
+        else if(Input.GetKeyDown(KeyCode.T) && outlineIsEnabled)
         {            
-            turnOffOutlines();
-            Debug.Log("Disable");
+            turnOffOutlines();            
+        }
+
+        if (Input.GetKeyDown(KeyCode.G) && !highlightIsEnabled)
+        {
+            highlightIsEnabled = true;
+            foreach (GameObject highlight in highlights)
+            {
+                highlight.SetActive(true);
+            }            
+        }
+        else if (Input.GetKeyDown(KeyCode.G) && highlightIsEnabled)
+        {
+            turnOffHighlights();            
         }
     }
 
     private void turnOffOutlines()
     {
-        isEnabled = false;
+        outlineIsEnabled = false;
         foreach (GameObject outline in outlines)
         {
             outline.SetActive(false);
         }        
+    }
+    private void turnOffHighlights()
+    {
+        highlightIsEnabled = false;
+        foreach (GameObject highlight in highlights)
+        {
+            highlight.SetActive(false);
+        }
     }
 }
