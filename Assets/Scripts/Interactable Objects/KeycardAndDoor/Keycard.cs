0bde4ec584b216e1c4380fdb613c1f237446c104
Smaksak <63716812+Smaksak@users.noreply.github.com>
2021-12-15 00:25:05 +0100

    AudioOnPickup (Keycards + Nanernaner :banana: )

--- Assets/Scripts/Interactable Objects/KeycardAndDoor/Keycard.cs
+++ Assets/Scripts/Interactable Objects/KeycardAndDoor/Keycard.cs
@@ -7,10 +7,12 @@ public class Keycard : MonoBehaviour
     private string Color;
     [SerializeField]
     private UnityEvent onPickup;
+    [SerializeField] private AudioClip pickupSound;
 
     private void OnTriggerEnter(Collider other) {
         if(other.CompareTag("Player"))
         {
+            gameObject.GetComponentInParent<AudioSource>().PlayOneShot(pickupSound, 1f);
             onPickup.Invoke();
             other.GetComponent<KeyChain>().AddKeyCard(Color);
             gameObject.SetActive(false);
