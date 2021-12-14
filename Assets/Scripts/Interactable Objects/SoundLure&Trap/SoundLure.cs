64a924a4f0f949f86fd6009cb1161d2f60cc5e78
Eaglebranch <williamornquist@gmail.com>
2021-12-14 02:22:43 +0100

    New enemy: Security Camera

--- Assets/Scripts/Interactable Objects/SoundLure&Trap/SoundLure.cs
+++ Assets/Scripts/Interactable Objects/SoundLure&Trap/SoundLure.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 [RequireComponent(typeof(Collider))]
 public class SoundLure : MonoBehaviour
 {
-    [Header("(Press Z to test activation in game)")]
-    [Space(20f)]
     private Collider lureCollider;
     [SerializeField, Range(0.1f, 3f), Tooltip("How many seconds the luring collider lasts upon activation.")]
     private float activityTime = 1f;
@@ -21,9 +19,6 @@ public class SoundLure : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Z))
-            ActivateLure();
-
         if (lureCollider.enabled && activityTimer < activityTime)
             activityTimer += Time.deltaTime;
         else if (lureCollider.enabled && activityTimer >= activityTime)
